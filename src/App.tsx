6c1c61bff50641bf838679e2e944fcf7f527591b
Zara Fatima Siddiqui <154442732+ZaraFatima123@users.noreply.github.com>
2025-04-14 01:26:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-// import React from 'react';
+// // import React from 'react';
+// import HomePage from './HomePage';
+
+// function App() {
+//   return (
+//     <HomePage />
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
 import HomePage from './HomePage';
 
 function App() {
+  const [showStudySchedule, setShowStudySchedule] = useState(false);
+
   return (
-    <HomePage />
+    <HomePage setShowStudySchedule={setShowStudySchedule} />
   );
 }
 
 export default App;
+
